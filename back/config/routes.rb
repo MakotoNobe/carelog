b032dec930818e85cb01ef629c3816e5192cf243
takehiro <ktwww00@gmail.com>
2021-06-12 09:16:39 +0900

    routes.rb変更

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'cities', to:'cities#get'
+      get ':prefecture_id/cities', to:'cities#get'
+      get ':area_id/:prefecture_id/cities', to:'cities#get'
     end
   end
 end
